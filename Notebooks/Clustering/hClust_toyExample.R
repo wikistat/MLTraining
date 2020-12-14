9b0abff9b1ca5fd2c88b8c0131b244345cc56551
Olivier Roustant <roustant@insa-toulouse.fr>
2020-12-14 12:26:54 +0100

    slides et jeude donnees velib

--- Notebooks/Clustering/hClust_toyExample.R
+++ Notebooks/Clustering/hClust_toyExample.R
@@ -1,11 +1,11 @@
 library(MASS)
 
-n <- 3
+n <- 30
 set.seed(1)
 x1 <- mvrnorm(n = n, mu = c(-5, 0), Sigma = diag(c(1,1)))
 x2 <- mvrnorm(n = n, mu = c(5, 0), Sigma = diag(c(1,1)))
 x3 <- cbind(seq(-3, 3, length.out = n), rep(0, n))
-x <- rbind(x1, x2) #, x3)
+x <- rbind(x1, x2, x3)
 N <- nrow(x)
 plot(x, type = "p", asp = 1)
 text(x[,1], x[,2], 1:N, pos = 2)
@@ -13,7 +13,7 @@ text(x[,1], x[,2], 1:N, pos = 2)
 # Try hierarchical clustering with different linkage functions
 # What's happening with "single" linkage?
 
-hc <- hclust(dist(x), method = "single")
+hc <- hclust(dist(x), method = "ward.D2")
 plot(hc)
 class <- cutree(hc, k = 2)
 class
